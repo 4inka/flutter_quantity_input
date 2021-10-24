9aba8e4573d6fc3bcc72de01b71a00a38115b307
Eunice Vassoa <eunicevassoa@gmail.com>
2021-10-24 13:29:25 +0200

    deleting unused files

--- test/src/quantity_input_for_int_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:quantity_input/quantity_input.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = QuantityInput();
-  });
-}
