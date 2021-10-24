43881da29d72e6c4de6c6a2eaa5d628be3a8086f
Eunice Vassoa <eunicevassoa@gmail.com>
2021-10-24 13:29:43 +0200

    importing files with name changes

--- lib/quantity_input.dart
+++ lib/quantity_input.dart
@@ -1,8 +1,9 @@
 library quantity_input;
 
-export 'src/quantity_input_for_int.dart';
-export 'src/quantity_input_for_double.dart';
+export 'src/quantity_input_as_int.dart';
+export 'src/quantity_input_as_double.dart';
 
 enum QuantityInputType {
-  withElevatedButton
+  withElevatedButton,
+  withIconButton,
 }
