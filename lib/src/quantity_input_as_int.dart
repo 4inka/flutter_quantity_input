ddc704381b5fcf5f1264b900aefee9df564c960e
Eunice Vassoa <eunicevassoa@gmail.com>
2021-11-20 19:24:14 +0200

    rewriting

--- lib/src/quantity_input_as_int.dart
+++ lib/src/quantity_input_as_int.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:quantity_input/widgets/icon-button.widget.dart';
 
 class QuantityInputAsInt extends StatefulWidget {
   final int value, step;
-  final Function(int) onButtonPress;
-  final Function(int)? onInput;
+  /// Set min value to be displayed in input. If not set, it will be set to 1
+  final int minValue;
+  /// ```dart
+  /// min(5, 3) == 3
+  /// ```
+  final Function(String) onChanged;
   final Color? buttonColor, iconColor;
   final String label;
-  final bool readOnly, acceptsNegatives;
+  final bool readOnly;
+  /// Default to false
+  /// 
+  /// Setting property to true enables the option to display negative values
+  final bool acceptsNegatives;
+  final TextEditingController? controller;
 
+  /// Test widget 1
   QuantityInputAsInt({
     this.value = 1,
     this.step = 1,
-    required this.onButtonPress,
-    this.onInput,
+    required this.onChanged,
     this.buttonColor,
     this.iconColor,
     this.label = '',
     this.readOnly = false,
-    this.acceptsNegatives = false
+    this.acceptsNegatives = false,
+    this.minValue = 1,
+    this.controller
   });
 
   @override
@@ -27,12 +39,23 @@ class QuantityInputAsInt extends StatefulWidget {
 }
 
 class _QuantityInputAsIntState extends State<QuantityInputAsInt> {
-  TextEditingController _controller = new TextEditingController();
+  late TextEditingController _controller;
+
+  String valueFormatter(int value) {
+    NumberFormat formatter = NumberFormat("#,###,###,###,###,###,###,###,###,##0", "en_US");
+    
+    return formatter.format(value);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? new TextEditingController();
+    _controller.text = valueFormatter(widget.value);
+  }
 
   @override
   Widget build(BuildContext context) {
-    _controller.text = widget.value.toString();
-
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 5),
       child: Column(
@@ -60,34 +83,38 @@ class _QuantityInputAsIntState extends State<QuantityInputAsInt> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              GestureDetector(
-                child: Container(
-                  child: Icon(
-                    Icons.remove,
-                    size: 25,
-                    color: widget.iconColor != null ? widget.iconColor : Colors.white
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: widget.buttonColor != null ? widget.buttonColor : Theme.of(context).primaryColor,
-                  ),
-                  width: 38,
-                  height: 38
-                ),
+              IconButtonWidget(
+                icon: Icons.remove,
+                iconColor: widget.iconColor,
+                buttonColor: widget.buttonColor,
                 onTap: () {
-                  if (widget.acceptsNegatives) widget.onButtonPress(-widget.step);
-                  else widget.value == 1 ? widget.onButtonPress(0) : widget.onButtonPress(-widget.step);
+                  int currentValue = 0;
+
+                  if (widget.acceptsNegatives || widget.value != 1)
+                    currentValue = widget.value - widget.step;
+                  else
+                    currentValue = widget.value;
+
+                  String formattedValue = valueFormatter(currentValue);
+                  _controller
+                    ..value = TextEditingValue(
+                      text: formattedValue,
+                      selection: TextSelection.collapsed(
+                        offset: formattedValue.length
+                      )
+                    );
+                  widget.onChanged(currentValue.toString());
                 }
               ),
               Container(
                 constraints: BoxConstraints(
                   maxHeight: 38,
-                  maxWidth:70,
+                  maxWidth:150,
                   minWidth: 50
                 ),
                 child: Padding(
                   padding: EdgeInsets.symmetric(horizontal: 5),
-                  child: TextField(
+                  child: TextFormField(
                     controller: _controller,
                     textAlign: TextAlign.center,
                     decoration: InputDecoration(
@@ -99,46 +126,52 @@ class _QuantityInputAsIntState extends State<QuantityInputAsInt> {
                       TextInputFormatter.withFunction((oldValue, newValue) {
                         if (newValue.text.contains(RegExp(r'[a-zA-Z]')))
                           return oldValue;
-                          
-                        var formatter = NumberFormat("#,###,###,###,###,###,###,###,###,###,###,###,###,##0", "en_US");
 
-                        String simpleValue = newValue.text
-                          .replaceAll(',', '');
+                        if (newValue.text.isEmpty)
+                          return TextEditingValue(
+                            text: '0',
+                            selection: TextSelection.collapsed(
+                              offset: 1
+                            )
+                          );
 
-                        String formattedValue = formatter.format(int.parse(simpleValue));
-                        
+                        String simpleValue = newValue.text.replaceAll(',', '');
+                        String formattedValue = valueFormatter(int.parse(simpleValue));
+
                         return TextEditingValue(
                           text: formattedValue,
                           selection: TextSelection.collapsed(
-                            offset: formattedValue.length
+                            offset: newValue.selection.baseOffset
                           )
                         );
                       })
                     ],
-                    onChanged: (value) => widget.onInput != null ? widget.onInput!(int.parse(value.replaceAll(',', ''))) : null
+                    onChanged: (value) => widget.onChanged(value.replaceAll(',', ''))
                   )
                 )
               ),
-              GestureDetector(
-                child: Container(
-                  child: Icon(
-                    Icons.add,
-                    size: 25,
-                    color: widget.iconColor != null ? widget.iconColor : Colors.white
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: widget.buttonColor != null ? widget.buttonColor : Theme.of(context).primaryColor,
-                  ),
-                  width: 38,
-                  height: 38
-                ),
-                onTap: () => widget.onButtonPress(widget.step),
+              IconButtonWidget(
+                icon: Icons.add,
+                iconColor: widget.iconColor,
+                buttonColor: widget.buttonColor,
+                onTap: () {
+                  int currentValue = widget.value + widget.step;
+                  String formattedValue = valueFormatter(currentValue);
+                  _controller
+                    ..value = TextEditingValue(
+                      text: formattedValue,
+                      selection: TextSelection.collapsed(
+                        offset: formattedValue.length
+                      )
+                    );
+                  widget.onChanged(currentValue.toString());
+                }
+                 
               )
-            ],
-          ),
-        ],
-      ),
+            ]
+          )
+        ]
+      )
     );
   }
 }
