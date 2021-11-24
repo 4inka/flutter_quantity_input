60c05dad69a427e22e0d157c398cc47526b6b10f
Eunice Vassoa <eunicevassoa@gmail.com>
2021-11-24 21:04:58 +0200

    adding custom elevation to button

--- lib/widgets/icon-button.widget.dart
+++ lib/widgets/icon-button.widget.dart
@@ -4,18 +4,20 @@ class IconButtonWidget extends StatelessWidget {
   final IconData icon;
   final Color? buttonColor, iconColor;
   final Function onTap;
+  final double elevation;
 
   const IconButtonWidget({
     required this.icon,
     this.buttonColor,
     this.iconColor,
-    required this.onTap
+    required this.onTap,
+    this.elevation = 3
   });
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      elevation: 3,
+      elevation: elevation,
       color: buttonColor ?? Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(5),
       child: InkWell(
